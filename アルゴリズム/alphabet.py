4cdeec9c71c7cd806071982e88a98a27b5ebfc56
Shunta Shimizu <askmis416@gmail.com>
2023-10-12 23:05:09 +0900

    import string

---
+++ アルゴリズム/alphabet.py
@@ -0,0 +1,12 @@
+import string
+
+# 小文字 abcdefghijklmnopqrstuvwxyz
+alphabet = string.ascii_lowercase
+
+# 大文字 ABCDEFGHIJKLMNOPQRSTUVWXYZ
+alphabet = string.ascii_uppercase
+
+# 小文字+大文字　abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+alphabet = string.ascii_letters
+
+print(alphabet)
